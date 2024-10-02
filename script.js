1794f5ae460b857b512df2514f65a3a8ebc95fdc
Nathanb_ecam <18092@ecam.be>
2024-10-02 14:42:26 +0200

    static timeline

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const elements = document.querySelectorAll('.animate-on-scroll');
 
-console.log("elements",elements)
+
 const observer = new IntersectionObserver((entries) => {
-    console.log("elements",elements)
     entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
